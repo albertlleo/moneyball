d1660790eef62520d4ba3635f4dd945bf3def144
Albert <albert.lleo3@gmail.com>
2019-10-03 20:49:32 +0200

    master

---
+++ PlayWiser.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 283 20:23:46 2019
+
+@author: Albert Lleo, Hamit, ...
+"""
+
+
+# Playwiser
+
+#We need to create a script-dialoge? or just with an example written we are done?
+
+import sys
+
+print('Hey here should start the dialoge, so hi')
+name = input("What's your name? ")
+print("Nice to meet you " + name + "!")
+age = input("Your age? ")
+print("So, you are already " + age + " years old, " + name + "!")
