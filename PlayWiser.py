f1a26e968684e4d48c77ae9403c708f72b4a73ab
Albert <albert.lleo3@gmail.com>
2019-10-04 17:51:28 +0200

    Update PlayWiser.py

--- PlayWiser.py
+++ PlayWiser.py
@@ -1,6 +1,4 @@
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
 """
 Created on Thu Oct 283 20:23:46 2019
 
@@ -14,24 +12,44 @@ Created on Thu Oct 283 20:23:46 2019
 
 import sys
 
+def search():
+	position = input("Alriht. Let's start defining the first position player you want. Is it DFC, MC or DL:  ") #guys its just supercoding, we need to define evttng and correct syntax lol
+	if position == "DFC":
+		char =  input("Alright, you need a " + position + "with more speed or with more strength?")
+		print("...")
+
+	elif position == "MC":
+		char =  input("Alright, you need a " + position + " with more speed. with more strength or vision?")
+		print("...")
+        
+	elif position == "DL":
+		char =  input("Alright, you need a " + position + " with more speed, with more strength or more precision?")
+		print("...")
+    
+#    else:
+#        print("Please choose a correct POS")
+#        search()
+        
+
+
+def printPlayer(x):
+
+	#print players vector, array, list.... idk
+	print("the caractheristics of" + player + "are") #printPLayer(player)
+#    return(player)
+    
+    
 print('Hi, wellcome to Playwiser! My name is Sussan and I will become your personal assistant on this search.')
 name = input("What's your name? ")
 print("Nice to meet you coach " + name + "!")
 answer1= input("Are you here to search for a player (search) or just to ask some habilities (habilities) of a player?")
-if answer1 is "search":
-	position = input("Alriht. Let's start defining the first position player you want. Is it DFC, MC or DL ") #guys its just supercoding, we need to define evttng and correct syntax lol
-	if position is "DFC":
-		print ("Alright, you need a" + position + "with more speed or with more strength?")
-		print("...")
 
-	elif position is "MC":
-		print ("Alright, you need a" + position + "with more speed. with more strength or vision?")
-		print("...")
-	
-	else:
-		print ("Alright, you need a" + position + "with more speed, with more strength or more precision?")
-		print("...")
+if answer1 == "search":
+	search()
+else:
+	player = input("Tell us the player you want to obtain features: ")
+	printPlayer(player)
 
 
 
-print("Alright " + name + " let's do ths sht (dude im just testing git desktop this is not official)")
+
