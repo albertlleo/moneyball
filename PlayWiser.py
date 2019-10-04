9be3d72244415d31c6260a3e0136e9f38b8600bb
Albert <albert.lleo3@gmail.com>
2019-10-04 17:20:09 +0200

    Update PlayWiser.py

--- PlayWiser.py
+++ PlayWiser.py
@@ -14,8 +14,24 @@ Created on Thu Oct 283 20:23:46 2019
 
 import sys
 
-print('Hey here should start the dialoge, so hi')
+print('Hi, wellcome to Playwiser! My name is Sussan and I will become your personal assistant on this search.')
 name = input("What's your name? ")
 print("Nice to meet you coach " + name + "!")
-age = input("Are you ready to choose your dream team man? ")
+answer1= input("Are you here to search for a player (search) or just to ask some habilities (habilities) of a player?")
+if answer1 is "search":
+	position = input("Alriht. Let's start defining the first position player you want. Is it DFC, MC or DL ") #guys its just supercoding, we need to define evttng and correct syntax lol
+	if position is "DFC":
+		print ("Alright, you need a" + position + "with more speed or with more strength?")
+		print("...")
+
+	elif position is "MC":
+		print ("Alright, you need a" + position + "with more speed. with more strength or vision?")
+		print("...")
+	
+	else:
+		print ("Alright, you need a" + position + "with more speed, with more strength or more precision?")
+		print("...")
+
+
+
 print("Alright " + name + " let's do ths sht (dude im just testing git desktop this is not official)")
