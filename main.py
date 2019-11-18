2c4221f5e0ae7f10e967a210532503d191029aa3
Marco Bertola <mrc.bertola@gmail.com>
2019-11-18 16:32:45 +0100

    added flat to use asr as input

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from asr import asr
 from tts import tts
+import argparse
 import spacy
 
 
@@ -15,12 +16,21 @@ ASR_MODE = 0
 
 def main():
 
-    speech = asr.processASR(ASR_MODE)
-    if speech:
-        tts.processTextToSpeech(speech)
+    parser = argparse.ArgumentParser(description='Assistant parameters')
+    parser.add_argument('--asr', action='store_true')
+    args = parser.parse_args()
+
+    input_text = ""
+    if args.asr :
+        input_text = asr.processASR(ASR_MODE)
+    else :
+        input_text = input("ask:")
+    
+    if input_text:
+        tts.processTextToSpeech(input_text)
 
     nlp = spacy.load('en')
-    doc = nlp(speech)
+    doc = nlp(input_text)
     for token in doc:
         print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}".format(
             token.text,
