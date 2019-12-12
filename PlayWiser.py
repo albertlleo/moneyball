20e715b06559b1a4b89275bb3cf07a89bef98893
Mr. A <amitsrivastava0191@gmail.com>
2019-12-12 14:09:23 +0100

    minor changes

--- PlayWiser.py
+++ PlayWiser.py
@@ -3,6 +3,9 @@ import pandas as pd
 import random
 
 
+query_intent = {'buy_type': '', 'attribute': [], 'quantifier': '', 'player_role':[]}
+
+
 def greeting():
 	print("Hi, My name is Susan and I am the best transfer market bot available")
 	name = input("What's your name, Coach?\n")
@@ -11,7 +14,9 @@ def greeting():
 	return query
 
 
-def dialogue(query_intent):
+def dialogue():
+	global query_intent
+	print("intent1", query_intent)
 	dialogues = pd.read_csv('dialogue.csv')
 	# If buy type is not specified , i.e., we don't know if he wants to buy or rent
 	if (query_intent['buy_type'] == 'find') | (query_intent['buy_type'] == ''):
@@ -19,38 +24,32 @@ def dialogue(query_intent):
 		# Picks a random dialogue from the same type of attributes
 		row = row.iloc[random.randint(0, row.shape[0] - 1)]
 		user_dialogue = input(row.dialogue)
-		buy_type, attribute, quantifier, player_role = nlp.process(user_dialogue)
-		query_intent = populate_intent(verb=buy_type, attribute=attribute, quantifier=quantifier, player_role=player_role)
-		print(query_intent)
 		# process new tokens
-
-	elif query_intent['buy_type'] == 'buy':
-		print(query_intent)
+		buy_type, attribute, quantifier, player_role = nlp.process(user_dialogue)
+		populate_intent(buy_type=buy_type, attribute=attribute, quantifier=quantifier, player_role=player_role)
+	print("intent2", query_intent)
+	if query_intent['buy_type'] == 'buy':
 		row = dialogues[dialogues.buy_type == 1]
 		# pick a random question to ask
+		row = row.iloc[random.randint(0, row.shape[0] - 1)]
+		user_dialogue = input(row.dialogue)
+	print("intent3", query_intent)
 
 
-def talk(query_intent):
-
-	if query_intent['buy_type'] == 'find':
-		query_intent['buy_type'] = input("Are you looking to buy or rent a player?")
-	if query_intent['player_role'] == ['']:
-		query_intent['player_role'] = input("What position are you looking for?")
-
-
-def populate_intent(verb='find', attribute='', quantifier='', player_role=''):
-	query_intent = {'buy_type': verb, 'attribute': [], 'quantifier': quantifier, 'player_role': []}
-	query_intent[attribute] = query_intent['attribute'].append(attribute)
-	query_intent[player_role] = query_intent['player_role'].append(attribute)
-	return query_intent
+def populate_intent(buy_type='', attribute='', quantifier='', player_role=''):
+	global query_intent
+	query_intent['buy_type'] = buy_type
+	query_intent['quantifier'] = quantifier
+	query_intent['attribute'].append(attribute)
+	query_intent['player_role'].append(player_role)
 
 
 def main():
+
 	query = greeting()
 	buy_type, attribute, quantifier, player_role = nlp.process(query)
-	query_intent = populate_intent(verb=buy_type, attribute=attribute, quantifier=quantifier, player_role=player_role)
-	#talk(query_intent=query_intent)
-	dialogue(query_intent)
+	populate_intent(buy_type=buy_type, attribute=attribute, quantifier=quantifier, player_role=player_role)
+	dialogue()
 
 
 if __name__ == '__main__':
