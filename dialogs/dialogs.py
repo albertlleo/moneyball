f3fddd690fa1378122130da2e28554fb30c6911f
Marco Bertola <mrc.bertola@gmail.com>
2019-12-14 18:08:03 +0100

    added new dialog

--- dialogs/dialogs.py
+++ dialogs/dialogs.py
@@ -2,6 +2,7 @@ ID_HELLO_WORLD = 1
 ID_EXAMPLE_WITH_PARAMETERS = 2
 ID_WELCOME = 3
 ID_SHOW_LIST_OF_STRIKER = 4
+ID_NO_VERB = 5
 
 
 class DialogManager:
@@ -25,5 +26,8 @@ class DialogManager:
             self.text = "Great! Let's move on to find a perfect player for you coach."
         if dial_id == ID_SHOW_LIST_OF_STRIKER:
             self.text = "This is the list of the {0} you are looking for".format(list_parameters[0])
+        if dial_id == ID_NO_VERB:
+            self.text = "I am sorry. I am not really understanding you. Remember that you can always ask me to find " \
+                        "or buy football players "
 
         self.saySomething()
