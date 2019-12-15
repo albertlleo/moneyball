d2824c3ca96333d6634ec020b61db697a025c856
hmtkvs <56079783+hmtkvs@users.noreply.github.com>
2019-12-15 19:21:48 +0100

    final filter

--- db/NFI.py
+++ db/NFI.py
@@ -9,12 +9,11 @@ import pandas as pd
 
 def get_request_players(context):
     df = pd.read_csv("db/NFIdatabase.csv", names=['name', 'age', 'overall',
-                                                  'value', 'potential', 'stamina', 'Nationality',
-                                                  'Club', 'International Reputation', 'Contract Valid Until',
-                                                  'Overall Score', 'speed',
-                                                  'diligence', 'Worth', 'Wage',
-                                                  'Value_Real', 'Wage_Real', 'Main_Position', 'position',
-                                                  'Release Clause'])
+                                               'value', 'potential', 'stamina', 'Nationality',
+                                               'Club', 'International Reputation', 'Contract Valid Until',
+                                               'Release Clause', 'Overall Score', 'speed',
+                                               'diligence', 'Worth', 'Wage',
+                                               'Value_Real', 'Wage_Real', 'Main_Position', 'position'])
 
     # context.has_player_name = True
     if context.has_player_name is False:
@@ -24,37 +23,38 @@ def get_request_players(context):
         usr_quant = context.quantifier_attribute
         usr_role = context.category_player_role
 
+        df = df[df['Value_Real'] != 0]
+        df['Value_Real'] = df['Value_Real'].astype(int)
+
+        df_filtered = df[df['Value_Real'] < int(usr_budget)]
+
+        df_output = df_filtered[(df_filtered[usr_attr] == usr_quant) & (df_filtered['position'] == usr_role)]
+        df_output = df_output.sort_values('overall', ascending=True)
+        print(df_output)
+        is_empty = df_output.empty
+        print(is_empty)
+        output = "EMPTY"
+        if not is_empty:
+
+            df_screen = df_output.loc[
+                df_output.index[:5], ['name', 'Worth', 'Overall Score', 'potential', 'Value_Real']]
+            df_screen['Overall Score'] = df_screen['Overall Score'].astype(str) + '/' + '100'
+            df_screen.columns = ['Player Name', 'Market Value', 'Overall Score', 'Player Talent', 'Value_Real']
+            df_screen = df_screen.sort_values('Value_Real', ascending=False)
+            df_screen = df_screen.reset_index()
+
+            output = df_screen.iloc[:, 1:-1].to_string()
+
+        return output
+
     else:
         usr_name = context.category_player
 
-    usr_attr = 'age'
-    usr_quant = 'low'
-    usr_role = 'striker'
-    usr_budget = 10000000
+    # usr_attr = 'age'
+    # usr_quant = 'low'
+    # usr_role = 'striker'
+    # usr_budget = 10000000
 
-    df = df[df['Value_Real'] != 0]
 
-    df['Value_Real'] = df['Value_Real'].astype(int)
-    df_filtered = df[df['Value_Real'] < int(usr_budget)]
-    df_output = df_filtered[(df_filtered[usr_attr] == usr_quant) & (df_filtered['position'] == usr_role)]
-    df_output = df_output.sort_values('overall', ascending=True)
-    print(df_output)
-
-    is_empty = df_output.empty
-    print(is_empty)
-    output = ""
-    if not is_empty:
-
-        df_screen = df_filtered.loc[
-            df_filtered.index[:5], ['name', 'Worth', 'Overall Score', 'potential', 'Release Clause', 'Value_Real']]
-        df_screen['Overall Score'] = df_screen['Overall Score'].astype(str) + '/' + '100'
-        df_screen.columns = ['Player Name', 'Market Value', 'Overall Score', 'Player Talent', 'Release Clause',
-                             'Value_Real']
-        df_screen = df_screen.sort_values('Value_Real', ascending=False)
-        df_screen = df_screen.reset_index()
-
-        output = df_screen.iloc[:, 1:-1].to_string()
-
-    return output
 
 
